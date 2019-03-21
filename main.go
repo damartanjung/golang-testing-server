df0b016bbcfa8a57345016a2ec7171ecceffca6b
damartanjung <damartanjung@gmail.com>
2019-03-21 16:01:26 +0700

    Implement RESTAPI with boolean and integer

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ import (
 )
 
 type Person struct {
-	ID        string    `json:"id,omitempty`
-	Firstname string    `json:"firstname,omitempty`
+	ID        string    `json:"id,omitempty"`
+	Firstname string    `json:"firstname,omitempty"`
 	Lastname  string    `json:"lastname,omitempty"`
 	CreatedAt time.Time `json:"createdat,omnitempty"`
 	Address   *Address  `json:"address,omitempty"`
+	IsDeleted bool      `json:"isdeleted,omitempty"`
+	Age       int       `json:"age,omnitempty"`
 }
 type Address struct {
 	City  string `json:"city,omitempty"`
@@ -46,6 +48,7 @@ func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	person.Firstname = params["firstame"]
 	person.Lastname = params["lastname"]
 	person.CreatedAt = time.Now()
+	person.IsDeleted = false
 	people = append(people, person)
 	json.NewEncoder(w).Encode(people)
 }
@@ -64,8 +67,8 @@ func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 // our main function
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", CreatedAt: time.Now(), Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", CreatedAt: time.Now(), Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", CreatedAt: time.Now(), Address: &Address{City: "City X", State: "State X"}, IsDeleted: false, Age: 10})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", CreatedAt: time.Now(), Address: &Address{City: "City Z", State: "State Y"}, IsDeleted: false, Age: 5})
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
